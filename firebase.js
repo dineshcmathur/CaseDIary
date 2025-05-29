af8d7469d68d7e07c8eb2e8a3ed8ab8e1ae2ba97
dineshcmathur <dineshcmathur@gmail.com>
2025-05-29 14:51:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from 'firebase/app';
+npm install
+  import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
